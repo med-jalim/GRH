78d2a8c1a6b0e2372ecdb5e152d24274590d59b7
abdellah2006-p <abdollahlasdos@gmail.com>
2026-03-30 13:06:02 +0100

    final

--- backend/app/Models/Hotel.php
+++ backend/app/Models/Hotel.php
@@ -10,5 +10,18 @@ class Hotel extends Model
 {
     protected $fillable = ['name', 'stars', 'ville', 'description'];
 
-   
+    public function chambres(): HasMany
+    {
+        return $this->hasMany(Chambre::class, 'id_hotel');
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class, 'id_hotel');
+    }
+
+    public function tarifs(): HasMany
+    {
+        return $this->hasMany(Tarif::class, 'id_hotel');
+    }
 }
